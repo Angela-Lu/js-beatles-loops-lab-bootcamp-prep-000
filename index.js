fb1cdde41fc40414599fe429b8701e28d14c21f0
Angela Lu <angela.lu0919@gmail.com>
2018-09-06 00:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
   var array = []
   do {
-    
+    array.push("I love the Beatles!")
   }
-  while(n < 15) {
-    array.
-  }
+  while(n < 15)
 }
